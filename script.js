37d08b74262ed192d848186d33386b1d6c416d5e
omkarshinde14 <114727827+omkarshinde14@users.noreply.github.com>
2024-03-19 22:57:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showtask() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
